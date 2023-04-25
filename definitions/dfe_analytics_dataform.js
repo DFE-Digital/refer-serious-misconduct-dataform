c4b564427ced07dee0f2884b32d83e645cc130f7
Paul Odeon <google-oauth2|112617077804035419503>
2023-04-25 13:17:25 +0000

    Correct Data Schema

--- definitions/dfe_analytics_dataform.js
+++ definitions/dfe_analytics_dataform.js
@@ -6,194 +6,535 @@ dfeAnalyticsDataform({
   bqDatasetName: "events_production",
   bqEventsTableName: "events",
   urlRegex: "refer-serious-misconduct.education.gov.uk", // re-2 formatted regular expression to use to identify whether a URL is this service's own URL or an external one. If your service only has one domain name set this to 'www.yourdomainname.gov.uk' (without the protocol). If you have more than one use something like '(?i)(www.domain1.gov.uk|www.domain2.gov.uk|www.domain3.gov.uk)'
-  dataSchema: [{
-    entityTableName: "events_prod",
-    description: "Description of this entity to include in metadata of denormalised tables produced for this entity.",
-    keys: [
+  dataSchema: [
   {
-    "name": "occurred_at",
-    "mode": "",
-    "type": "TIMESTAMP",
-    "description": "The timestamp at which the event occurred in the application.",
-    "fields": []
-  },
-  {
-    "name": "request_uuid",
-    "mode": "",
-    "type": "STRING",
-    "description": "UUID of the web request that either is this event, or that caused this event.",
-    "fields": []
-  },
-  {
-    "name": "event_type",
-    "mode": "",
-    "type": "STRING",
-    "description": "The type of the event, for example web_request. This determines the schema of the data which will be included in the data field.",
-    "fields": []
-  },
-  {
-    "name": "environment",
-    "mode": "",
-    "type": "STRING",
-    "description": "The application environment that the event was streamed from.",
-    "fields": []
-  },
-  {
-    "name": "namespace",
-    "mode": "",
-    "type": "STRING",
-    "description": "The namespace of the instance of dfe-analytics that streamed this event. For example this might identify the name of the service that streamed the event.",
-    "fields": []
-  },
-  {
-    "name": "data",
-    "mode": "REPEATED",
-    "type": "RECORD",
-    "description": "ARRAY of STRUCTs, each with a key and a value. Contains a set of data points appropriate to the event_type of this event. For example, if this event was an entity create, update, delete or import event, data will contain the values of each field in the database after this event took place - according to the settings in the analytics.yml configured for this instance of dfe-analytics. Value be anonymised as a one way hash, depending on configuration settings.",
-    "fields": [
-      {
-        "name": "key",
-        "mode": "",
-        "type": "STRING",
-        "description": null,
-        "fields": []
-      },
-      {
-        "name": "value",
-        "mode": "REPEATED",
-        "type": "STRING",
-        "description": null,
-        "fields": []
-      }
-    ]
-  },
-  {
-    "name": "entity_table_name",
-    "mode": "",
-    "type": "STRING",
-    "description": "If event_type was an entity create, update, delete or import event, the name of the table in the database that this entity is stored in. NULL otherwise.",
-    "fields": []
-  },
-  {
-    "name": "request_path",
-    "mode": "",
-    "type": "STRING",
-    "description": "The path, starting with a / and excluding any query parameters, of the web request that either is this event, or caused this event.",
-    "fields": []
-  },
-  {
-    "name": "request_user_id",
-    "mode": "",
-    "type": "STRING",
-    "description": "If a user was logged in when they sent a web request event that is, or caused, this event, then this is the UID of this user.",
-    "fields": []
-  },
-  {
-    "name": "request_method",
-    "mode": "",
-    "type": "STRING",
-    "description": "Whether the web request that either is this event, or caused this event, was a GET or a POST request.",
-    "fields": []
-  },
-  {
-    "name": "request_user_agent",
-    "mode": "",
-    "type": "STRING",
-    "description": "The user agent of the web request that either is this event or caused this event. Allows a user's browser and operating system to be identified.",
-    "fields": []
-  },
-  {
-    "name": "request_referer",
-    "mode": "",
-    "type": "STRING",
-    "description": "The URL of any page the user was viewing when they initiated the web request that either is this event or caused this event. This is the full URL, including protocol (https://) and any query parameters, if the browser shared these with our application as part of the web request. It is very common for this referer to be truncated for referrals from external sites.",
-    "fields": []
-  },
-  {
-    "name": "request_query",
-    "mode": "REPEATED",
-    "type": "RECORD",
-    "description": "ARRAY of STRUCTs, each with a key and a value. Contains any query parameters that were sent to the application as part of the web request that was this event or caused this event.",
-    "fields": [
-      {
-        "name": "key",
-        "mode": "",
-        "type": "STRING",
-        "description": null,
-        "fields": []
-      },
-      {
-        "name": "value",
-        "mode": "REPEATED",
-        "type": "STRING",
-        "description": null,
-        "fields": []
-      }
-    ]
-  },
-  {
-    "name": "response_status",
-    "mode": "",
-    "type": "STRING",
-    "description": "HTTP response code returned by the application in response to the web request that either was this event or caused this event. See https://developer.mozilla.org/en-US/docs/Web/HTTP/Status.",
-    "fields": []
-  },
-  {
-    "name": "response_content_type",
-    "mode": "",
-    "type": "STRING",
-    "description": "Content type of any data that was returned to the browser following the web request that either was this event or caused this event. For example, 'text/html; charset=utf-8'. Image views, for example, may have a non-text/html content type.",
-    "fields": []
-  },
-  {
-    "name": "anonymised_user_agent_and_ip",
-    "mode": "",
-    "type": "STRING",
-    "description": "One way hash of a combination of the user's IP address and user agent. Can be used to identify the user anonymously, even when user_id is not set. Cannot be used to identify the user over a time period of longer than about a month, because of IP address changes and browser updates.",
-    "fields": []
-  },
-  {
-    "name": "device_category",
-    "mode": "",
-    "type": "STRING",
-    "description": "The category of device used to cause this event - desktop, mobile, bot or unknown.",
-    "fields": []
-  },
-  {
-    "name": "browser_name",
-    "mode": "",
-    "type": "STRING",
-    "description": "The name of the browser used to cause this event.",
-    "fields": []
-  },
-  {
-    "name": "browser_version",
-    "mode": "",
-    "type": "STRING",
-    "description": "The version of the browser used to cause this event.",
-    "fields": []
-  },
-  {
-    "name": "operating_system_name",
-    "mode": "",
-    "type": "STRING",
-    "description": "The name of the operating system used to cause this event.",
-    "fields": []
-  },
-  {
-    "name": "operating_system_vendor",
-    "mode": "",
-    "type": "STRING",
-    "description": "The vendor of the operating system used to cause this event.",
-    "fields": []
-  },
-  {
-    "name": "operating_system_version",
-    "mode": "",
-    "type": "STRING",
-    "description": "The version of the operating system used to cause this event.",
-    "fields": []
-  }
-]
+  entityTableName: "active_storage_attachments",
+  description: "",
+  keys: [{
+     keyName: "blob_id",
+     dataType: "integer",
+     description: ""
+  }, {
+     keyName: "name",
+     dataType: "string",
+     description: ""
+  }, {
+     keyName: "record_id",
+     dataType: "integer",
+     description: ""
+  }, {
+     keyName: "record_type",
+     dataType: "string",
+     description: ""
   }]
+},
+{
+   entityTableName: "active_storage_blobs",
+   description: "",
+   keys: [{
+      keyName: "byte_size",
+      dataType: "integer",
+      description: ""
+   }, {
+      keyName: "checksum",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "content_type",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "filename",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "key",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "metadata",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "service_name",
+      dataType: "string",
+      description: ""
+   }]
+},
+{
+   entityTableName: "eligibility_checks",
+   description: "",
+   keys: [{
+      keyName: "complained",
+      dataType: "boolean",
+      description: ""
+   }, {
+      keyName: "is_teacher",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "reporting_as",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "serious_misconduct",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "teaching_in_england",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "unsupervised_teaching",
+      dataType: "string",
+      description: ""
+   }]
+},
+{
+   entityTableName: "referral_evidences",
+   description: "",
+   keys: [{
+      keyName: "filename",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "referral_id",
+      dataType: "integer",
+      description: ""
+   }]
+},
+{
+   entityTableName: "referrals",
+   description: "",
+   keys: [{
+      keyName: "address_known",
+      dataType: "boolean",
+      description: ""
+   }, {
+      keyName: "address_line_1",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "address_line_2",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "age_known",
+      dataType: "boolean",
+      description: ""
+   }, {
+      keyName: "allegation_consideration_details",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "allegation_details",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "allegation_details_complete",
+      dataType: "boolean",
+      description: ""
+   }, {
+      keyName: "allegation_format",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "contact_details_complete",
+      dataType: "boolean",
+      description: ""
+   }, {
+      keyName: "country",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "date_of_birth",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "dbs_notified",
+      dataType: "boolean",
+      description: ""
+   }, {
+      keyName: "duties_details",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "duties_format",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "eligibility_check_id",
+      dataType: "integer",
+      description: ""
+   }, {
+      keyName: "email_address",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "email_known",
+      dataType: "boolean",
+      description: ""
+   }, {
+      keyName: "employment_status",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "evidence_details_complete",
+      dataType: "boolean",
+      description: ""
+   }, {
+      keyName: "first_name",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "has_evidence",
+      dataType: "boolean",
+      description: ""
+   }, {
+      keyName: "has_qts",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "job_title",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "last_name",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "name_has_changed",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "ni_number",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "ni_number_known",
+      dataType: "boolean",
+      description: ""
+   }, {
+      keyName: "organisation_address_known",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "organisation_address_line_1",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "organisation_address_line_2",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "organisation_name",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "organisation_postcode",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "organisation_town_or_city",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "personal_details_complete",
+      dataType: "boolean",
+      description: ""
+   }, {
+      keyName: "phone_known",
+      dataType: "boolean",
+      description: ""
+   }, {
+      keyName: "phone_number",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "postcode",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "previous_misconduct_complete",
+      dataType: "boolean",
+      description: ""
+   }, {
+      keyName: "previous_misconduct_details",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "previous_misconduct_format",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "previous_misconduct_reported",
+      dataType: "boolean",
+      description: ""
+   }, {
+      keyName: "previous_name",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "reason_leaving_role",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "role_end_date",
+      dataType: "date",
+      description: ""
+   }, {
+      keyName: "role_end_date_known",
+      dataType: "boolean",
+      description: ""
+   }, {
+      keyName: "role_start_date",
+      dataType: "date",
+      description: ""
+   }, {
+      keyName: "role_start_date_known",
+      dataType: "boolean",
+      description: ""
+   }, {
+      keyName: "same_organisation",
+      dataType: "boolean",
+      description: ""
+   }, {
+      keyName: "submitted_at",
+      dataType: "timestamp",
+      description: ""
+   }, {
+      keyName: "teacher_role_complete",
+      dataType: "boolean",
+      description: ""
+   }, {
+      keyName: "town_or_city",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "trn",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "trn_known",
+      dataType: "boolean",
+      description: ""
+   }, {
+      keyName: "user_id",
+      dataType: "integer",
+      description: ""
+   }, {
+      keyName: "work_address_line_1",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "work_address_line_2",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "work_location_known",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "work_organisation_name",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "work_postcode",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "work_town_or_city",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "working_somewhere_else",
+      dataType: "string",
+      description: ""
+   }]
+},
+{
+   entityTableName: "referrers",
+   description: "",
+   keys: [{
+      keyName: "complete",
+      dataType: "boolean",
+      description: ""
+   }, {
+      keyName: "first_name",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "job_title",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "last_name",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "phone",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "referral_id",
+      dataType: "integer",
+      description: ""
+   }]
+},
+{
+   entityTableName: "staff",
+   description: "",
+   keys: [{
+      keyName: "confirmation_sent_at",
+      dataType: "timestamp",
+      description: ""
+   }, {
+      keyName: "confirmed_at",
+      dataType: "timestamp",
+      description: ""
+   }, {
+      keyName: "current_sign_in_at",
+      dataType: "timestamp",
+      description: ""
+   }, {
+      keyName: "current_sign_in_ip",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "deleted_at",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "email",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "failed_attempts",
+      dataType: "integer",
+      description: ""
+   }, {
+      keyName: "invitation_accepted_at",
+      dataType: "timestamp",
+      description: ""
+   }, {
+      keyName: "invitation_created_at",
+      dataType: "timestamp",
+      description: ""
+   }, {
+      keyName: "invitation_limit",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "invitation_sent_at",
+      dataType: "timestamp",
+      description: ""
+   }, {
+      keyName: "invitations_count",
+      dataType: "integer",
+      description: ""
+   }, {
+      keyName: "invited_by_id",
+      dataType: "integer",
+      description: ""
+   }, {
+      keyName: "invited_by_type",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "last_sign_in_at",
+      dataType: "timestamp",
+      description: ""
+   }, {
+      keyName: "last_sign_in_ip",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "locked_at",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "manage_referrals",
+      dataType: "boolean",
+      description: ""
+   }, {
+      keyName: "remember_created_at",
+      dataType: "timestamp",
+      description: ""
+   }, {
+      keyName: "reset_password_sent_at",
+      dataType: "timestamp",
+      description: ""
+   }, {
+      keyName: "sign_in_count",
+      dataType: "integer",
+      description: ""
+   }, {
+      keyName: "unconfirmed_email",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "view_support",
+      dataType: "boolean",
+      description: ""
+   }]
+},
+{
+   entityTableName: "users",
+   description: "",
+   keys: [{
+      keyName: "current_sign_in_at",
+      dataType: "timestamp",
+      description: ""
+   }, {
+      keyName: "current_sign_in_ip",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "email",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "last_sign_in_at",
+      dataType: "timestamp",
+      description: ""
+   }, {
+      keyName: "last_sign_in_ip",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "otp_created_at",
+      dataType: "timestamp",
+      description: ""
+   }, {
+      keyName: "otp_guesses",
+      dataType: "integer",
+      description: ""
+   }, {
+      keyName: "remember_created_at",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "sign_in_count",
+      dataType: "integer",
+      description: ""
+   }, {
+      keyName: "uuid",
+      dataType: "string",
+      description: ""
+   }]
+},
+{
+   entityTableName: "validation_errors",
+   description: "",
+   keys: [{
+      keyName: "details",
+      dataType: "string",
+      description: ""
+   }, {
+      keyName: "form_object",
+      dataType: "string",
+      description: ""
+   }]
+}  
+  ]
 });
